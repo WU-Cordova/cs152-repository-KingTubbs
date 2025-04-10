dc0846c134fe5440ccf2bd04575d2c131f28f5e4
KingTubbs <kayadon@willamette.edu>
2025-04-09 22:08:32 -0700

    linked lists completed

--- program.py
+++ program.py
@@ -1,408 +1,101 @@
-from typing import Any
+from __future__ import annotations
 
-from datastructures.array import Array
-from datastructures.iqueue import IQueue, T
+from dataclasses import dataclass
+import os
+from typing import Optional, Sequence
+from datastructures.ilinkedlist import ILinkedList, T
 
-class CircularQueue(IQueue[T]):
-    """ Represents a fixed-size circular queue. The queue
-        is circular in the sense that the front and rear pointers wrap around the
-        array when they reach the end. The queue is full when the rear pointer is
-        one position behind the front pointer. The queue is empty when the front
-        and rear pointers are equal. This implementation uses a fixed-size array.
-    """
 
-    def __init__(self, maxsize: int = 0, data_type=object) -> None:
-        ''' Initializes the CircularQueue object with a maxsize and data_type.
-        
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.maxsize
-                5
-                >>> q.empty
-                True
-                >>> q.full
-                False
-                >>> q.front
-                IndexError('Queue is empty')
-                >>> q.rear
-                IndexError('Queue is empty')
+class LinkedList[T](ILinkedList[T]):
 
-            Arguments:
-                maxsize: The maximum size of the queue
-                data_type: The type of the elements in the queue
-        '''
-        self._maxsize = maxsize
-        self.data_type = data_type
-        self.starting_sequence = [data_type() for i in range(maxsize + 1)]
-        self.circularqueue = Array(self.starting_sequence, data_type)
-        self.default = data_type()
-        self._front = 0
-        self._rear = 0
-        self._number_of_items = 0
+    @dataclass
+    class Node:
+        data: T
+        next: Optional[LinkedList.Node] = None
+        previous: Optional[LinkedList.Node] = None
 
-    def enqueue(self, item: T) -> None:
-        ''' Adds an item to the rear of the queue
+    def __init__(self, data_type: type = object) -> None:
+        self.list = []
+        head = self.Node(data=data_type)
+        self.nodes.append(head)
 
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> q.front
-                1
-                >>> q.rear
-                3
-                >>> q.enqueue(4)
-                >>> q.enqueue(5)
-                >>> q.full
-                True
-                >>> q.enqueue(6)
-                IndexError('Queue is full')
-            
-            Arguments:
-                item: The item to add to the queue
-                
-            Raises:
-                IndexError: If the queue is full
-        '''
-        if self.full:
-            raise IndexError
-        self.circularqueue[self._rear] = item
-        self._rear = (self._rear + 1) % len(self.circularqueue)
-        self._number_of_items += 1
+    @staticmethod
+    def from_sequence(sequence: Sequence[T], data_type: type=object) -> LinkedList[T]:
+        raise NotImplementedError("LinkedList.from_sequence is not implemented")
 
-    def dequeue(self) -> T:
-        ''' Removes and returns the item at the front of the queue
+    def append(self, item: T) -> None:
+        raise NotImplementedError("LinkedList.append is not implemented")
 
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> q.dequeue()
-                1
-                >>> q.dequeue()
-                2
-                >>> q.dequeue()
-                3
-                >>> q.dequeue()
-                IndexError('Queue is empty')
-                >>> q.dequeue()
-                IndexError('Queue is empty')
+    def prepend(self, item: T) -> None:
+        raise NotImplementedError("LinkedList.prepend is not implemented")
 
-            Returns:
-                The item at the front of the queue
+    def insert_before(self, target: T, item: T) -> None:
+        raise NotImplementedError("LinkedList.insert_before is not implemented")
 
-            Raises:
-                IndexError: If the queue is empty
-        '''
-        if self.empty:
-            raise IndexError
-        item = self.circularqueue[self._front]
-        self.circularqueue[self._front] = self.default
-        self._front = (self._front + 1) % len(self.circularqueue)
-        self._number_of_items -= 1
-        return item
+    def insert_after(self, target: T, item: T) -> None:
+        raise NotImplementedError("LinkedList.insert_after is not implemented")
 
-    def clear(self) -> None:
-        ''' Removes all items from the queue 
-        
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> q.clear()
-                >>> q.empty
-                True
-                >>> q.front
-                IndexError('Queue is empty')
-                >>> q.rear
-                IndexError('Queue is empty')
-        '''
-        self.circularqueue = Array(self.starting_sequence, self.data_type)
-        self._front = 0
-        self._rear = 0
-        self._number_of_items = 0
+    def remove(self, item: T) -> None:
+        raise NotImplementedError("LinkedList.remove is not implemented")
+
+    def remove_all(self, item: T) -> None:
+        raise NotImplementedError("LinkedList.remove_all is not implemented")
+
+    def pop(self) -> T:
+        raise NotImplementedError("LinkedList.pop is not implemented")
+
+    def pop_front(self) -> T:
+        raise NotImplementedError("LinkedList.pop_front is not implemented")
 
     @property
     def front(self) -> T:
-        ''' Returns the item at the front of the queue without removing it
-
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> q.front
-                1
-                >>> q.dequeue()
-                1
-                >>> q.front
-                2
-                >>> q.dequeue()
-                2
-                >>> q.front
-                3
-                >>> q.dequeue()
-                3
-                >>> q.front
-                IndexError('Queue is empty')
-
-            Returns:
-                The item at the front of the queue
-
-            Raises:
-                IndexError: If the queue is empty
-        '''
-        if self.empty:
-            raise IndexError
-        item = self.circularqueue[self._front]
-        return item
+        raise NotImplementedError("LinkedList.front is not implemented")
 
     @property
-    def full(self) -> bool:
-        ''' Returns True if the queue is full, False otherwise 
-
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.full
-                False
-                >>> q.enqueue(1)
-                >>> q.full
-                False
-                >>> q.enqueue(2)
-                >>> q.full
-                False
-                >>> q.enqueue(3)
-                >>> q.full
-                False
-                >>> q.enqueue(4)
-                >>> q.full
-                False
-                >>> q.enqueue(5)
-                >>> q.full
-                True
-                >>> q.dequeue()
-                1
-                >>> q.full
-                False
-                >>> q.dequeue()
-                2
-                >>> q.full
-                False
-                >>> q.dequeue()
-                3
-                >>> q.full
-                False
-                >>> q.dequeue()
-                4
-                >>> q.full
-                False
-                >>> q.dequeue()
-                5
-                >>> q.full
-                False
-        
-            Returns:
-                True if the queue is full, False otherwise
-        '''
-        return self._number_of_items == self._maxsize
+    def back(self) -> T:
+        raise NotImplementedError("LinkedList.back is not implemented")
 
     @property
     def empty(self) -> bool:
-        ''' Returns True if the queue is empty, False otherwise
-
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.empty
-                True
-                >>> q.enqueue(1)
-                >>> q.empty
-                False
-                >>> q.enqueue(2)
-                >>> q.empty
-                False
-                >>> q.enqueue(3)
-                >>> q.empty
-                False
-                >>> q.enqueue(4)
-                >>> q.empty
-                False
-                >>> q.enqueue(5)
-                >>> q.empty
-                False
-                >>> q.dequeue()
-                1
-                >>> q.empty
-                False
-                >>> q.dequeue()
-                2
-                >>> q.empty
-                False
-                >>> q.dequeue()
-                3
-                >>> q.empty
-                False
-                >>> q.dequeue()
-                4
-                >>> q.empty
-                False
-                >>> q.dequeue()
-                5
-                >>> q.empty
-        
-            Returns:
-                True if the queue is empty, False otherwise
-        '''
-        return self._number_of_items == 0
-    
-    @property
-    def maxsize(self) -> int:
-        ''' Returns the maximum size of the queue
-
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.maxsize
-                5
-
-            Returns:
-                The maximum size of the queue
-        '''
-        return len(self.circularqueue)
-
-    def __eq__(self, other: object) -> bool:
-        ''' Returns True if this CircularQueue is equal to another object, False otherwise
-        
-            Equality is defined as:
-                - The element values at the front and rear pointers are equal
-                - The element values between the front and rear pointers are equal
-                - The maxsize of the queue is equal
-                - The data_type of the queue is equal
-                - Two queues are equal if they have the same elements in the same order, regardless of the index
-                  of the front and rear pointers.
-
-            Examples:
-                >>> q1 = CircularQueue(maxsize=5, data_type=int)
-                >>> q2 = CircularQueue(maxsize=5, data_type=int)
-                >>> q1 == q2
-                True
-                >>> for i in range(5): q1.enqueue(i)
-                >>> for i in range(5): q2.enqueue(i)
-                >>> q1 == q2
-                True
-                >>> q1.dequeue()
-                0
-                >>> q1 == q2
-                False
-                
-            Arguments:
-                other: The object to compare this CircularQueue to
-                
-            Returns:
-                True if this CircularQueue is equal to another object, False otherwise
-        '''
-        if not isinstance(other, CircularQueue) or len(self) != len(other):
-            return False
-        
-        self_items = []
-        other_items = []
-
-        for i in range(len(self.circularqueue)):
-            self_items.append(self.circularqueue[(self._front + i) % len(self.circularqueue)])
-            other_items.append(other.circularqueue[(other._front + i) % len(other.circularqueue)])
-
-        #print(self_items)
-        #print(other_items)
-
-        for i in range(len(self_items)):
-            if self_items[i] != other_items[i]:
-                return False
-        else:
-            return True
+        raise NotImplementedError("LinkedList.empty is not implemented")
 
     def __len__(self) -> int:
-        ''' Returns the number of items in the queue
+        raise NotImplementedError("LinkedList.__len__ is not implemented")
 
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> len(q)
-                0
-                >>> q.enqueue(1)
-                >>> len(q)
-                1
-                >>> q.enqueue(2)
-                >>> len(q)
-                2
-                >>> q.enqueue(3)
-                >>> len(q)
-                3
-                >>> q.dequeue()
-                1
-                >>> len(q)
-                2
-                >>> q.dequeue()
-                2
-                >>> len(q)
-                1
-                >>> q.dequeue()
-                3
-                >>> len(q)
-                0
-        
-            Returns:
-                The number of items in the queue
-        '''
-        return self._number_of_items
-        
+    def clear(self) -> None:
+        raise NotImplementedError("LinkedList.clear is not implemented")
+
+    def __contains__(self, item: T) -> bool:
+        raise NotImplementedError("LinkedList.__contains__ is not implemented")
+
+    def __iter__(self) -> ILinkedList[T]:
+        raise NotImplementedError("LinkedList.__iter__ is not implemented")
+
+    def __next__(self) -> T:
+        raise NotImplementedError("LinkedList.__next__ is not implemented")
+    
+    def __reversed__(self) -> ILinkedList[T]:
+        raise NotImplementedError("LinkedList.__reversed__ is not implemented")
+    
+    def __eq__(self, other: object) -> bool:
+        raise NotImplementedError("LinkedList.__eq__ is not implemented")
 
     def __str__(self) -> str:
-        ''' Returns a string representation of the CircularQueue
-
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> print(q)
-                [1, 2, 3]
-        
-            Returns:
-                A string representation of the queue
-        '''
-        return str(self.circularqueue)
+        items = []
+        current = self.head
+        while current:
+            items.append(repr(current.data))
+            current = current.next
+        return '[' + ', '.join(items) + ']'
 
     def __repr__(self) -> str:
-        ''' Returns a developer string representation of the CircularQueue object
+        items = []
+        current = self.head
+        while current:
+            items.append(repr(current.data))
+            current = current.next
+        return f"LinkedList({' <-> '.join(items)}) Count: {self.count}"
 
-            Examples:
-                >>> q = CircularQueue(maxsize=5, data_type=int)
-                >>> q.enqueue(1)
-                >>> q.enqueue(2)
-                >>> q.enqueue(3)
-                >>> repr(q)
-                'ArrayQueue([1, 2, 3])'
-        
-            Returns:
-                A string representation of the CircularQueue object
-        '''
-        return f'ArrayQueue({repr(self.circularqueue)})'
-                                  
-
-    
 if __name__ == '__main__':
-    q1 = CircularQueue(maxsize=5, data_type=int)
-    q2 = CircularQueue(maxsize=5, data_type=int)
-    for i in range(5):
-        q1.enqueue(i)
-        
-    for i in range(3):
-        q2.enqueue(i)
-        
-    for i in range(3):
-        q2.dequeue()
-    
-    for i in range(5):
-        q2.enqueue(i)
-
-    assert q1 == q2
+    linked_list = LinkedList(data_type=int)
+    print(linked_list)
